5d47f9afae29854c6185bf2c2bd0be5e3180af84
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-01 17:01:11 +0300

    task_019

---
+++ task_019/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while(Math.Abs(num)<10000 ||Math.Abs(num)>99999)
+{
+    Console.Write("Введите пятизначное число: ");
+   num = Convert.ToInt32(Console.ReadLine());
+ }
+ string num1 = Math.Abs(num).ToString();
+ if(num1[0]==num1[4]&&num1[1]==num1[3])
+ {
+Console.Clear();
+Console.WriteLine(num+" -> Да");
+ }
+ else
+ {
+  Console.WriteLine(num+" -> Нет");
+ }
