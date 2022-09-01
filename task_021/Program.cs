71a2298f25a934ef5655212b4833c9aad3ee0dc0
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-01 17:07:41 +0300

    task_021

---
+++ task_021/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите координату х точки A: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y точки A: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z точки A: ");
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату х точки B: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y точки B: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z точки B: ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.Write("A("+xA+","+yA+","+zA+"); B("+xB+","+yB+","+zB+") -> ");
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow((xB-xA),2)+Math.Pow((yB-yA),2)+Math.Pow((zB-zA),2)),2));
