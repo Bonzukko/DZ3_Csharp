e93a4453d6788e0a1a16f0b698cd97a8cd3bd49b
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-01 17:12:45 +0300

    task_023

---
+++ task_023/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write(N + " -> ");
+
+if(N >= 1)
+{for(int index = 1; index <= N; index++)
+{
+    Console.Write(Math.Pow(index,3) + ", ");
+}
+}
+else
+{for(int index = 0; index >= N; index = index - 1)
+{
+    Console.Write(Math.Pow(index, 3) + ", ");
+}
+}
